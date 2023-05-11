177d621a90b21abaea1a02d872476f0766e495f5
ali <aliebrahimi2079@gmail.com>
2023-05-12 02:33:05 +0330

    Complete product data access module

---
+++ dataAccess/product.js
@@ -0,0 +1,35 @@
+const privates = {
+  products: [],
+};
+
+const productDataAccess = {
+  reset() {
+    privates.products = [];
+  },
+
+  dbs() {
+    return {
+      products: privates.products,
+    };
+  },
+
+  insertProduct(productData) {
+    let product = privates.products.find(p => p.name === productData.name);
+    if (product) {
+      product.price = productData.price;
+    } else {
+      product = {
+        name: productData.name,
+        price: productData.price,
+      };
+      privates.products.push(product);
+    }
+    return product;
+  },
+
+  fetchProductByName(name) {
+    return privates.products.find(p => p.name === name);
+  },
+};
+
+module.exports = productDataAccess;
