85d0efdd955d667619479f88031952dec029f7e3
ali <aliebrahimi2079@gmail.com>
2023-05-12 03:03:34 +0330

    Complete product class and repository

---
+++ repository/ProductRepository.js
@@ -0,0 +1,19 @@
+const productDataAccess = require('../dataAccess/product');
+const Product = require('../domain/Product');
+
+
+class ProductRepository {
+  static save(product) {
+    return productDataAccess.insertProduct(product);
+  }
+
+  static getByName(name) {
+    const productData = productDataAccess.getByName(name);
+    if (productData) {
+      return new Product(productData.name, productData.price);
+    }
+    throw new Error('PRODUCT NOT FOUND!', name);
+  }
+};
+
+module.exports = productRepository;
